14d1e6e073bb9825ab0efb28d8923cbe844e279d
Hamza Baig <hamzabaig449@gmail.com>
2021-09-01 20:49:27 +0500

    Database changes

--- helpers/helpers.js
+++
@@ -1,8 +0,0 @@
-const validator = require("validator");
-
-exports.sanitizeData = (data)=>{
-    for (const key in data) {
-        data[key] = validator.trim(data[key]);
-        data[key] = validator.escape(data[key]);
-    }
-}
